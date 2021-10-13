2f80305fab5d3e0e67538ac5f96b1a445f942359
Deltax2016 <deltaxussr@gmail.com>
2021-10-13 06:48:36 +0300

    added firstN

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI
 from app.scripts.getTop import getTopBalances
 from app.scripts.getBalance import getBalances
+from app.scripts.firstN import getFirstWallets
 
 app = FastAPI()
 
 
 @app.get("/api/get_top_days/")
-async def get_top_days(start: int = 1609459200, days: int = 1, amount: int = 10000):
+async def get_top_days(start: int = 1617291702, days: int = 1, amount: int = 10000):
 
     response  = await getTopBalances(start, days, amount)
     return {"data":response}
 
 @app.get("/api/get_dao_days/")
-async def get_dao_days(start: int = 1609459200, days: int = 1):
+async def get_dao_days(start: int = 1617291702, days: int = 1):
     wallet = "0x245cc372C84B3645Bf0Ffe6538620B04a217988B"
     response  = await getBalances(start, days, wallet)
+    return {"data":response}
+
+@app.get("/api/get_first_n/")
+async def get_first_n(start: int = 1617291702, days: int = 1, count: int = 1):
+    response  = await getFirstWallets(start, days, count)
     return {"data":response}
---
+++ app/scripts/firstN.py
@@ -0,0 +1,63 @@
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+from datetime import datetime
+
+transport = AIOHTTPTransport(url="https://api.thegraph.com/subgraphs/name/deltax2016/olympus-wallets")
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+async def getFirstWallets(timestamp_start, period, cnt=None):
+    
+    day_start = datetime.fromtimestamp(timestamp_start).timetuple().tm_yday
+    timestamp_end = timestamp_start + 86400*period
+
+    queryString = f"""query balancesByWallet {{
+        wallets(orderBy: birth, first: {cnt}) {{ 
+            id
+            dailyBalance(orderBy: timestamp) {{
+                ohmBalance
+                day
+            }}
+        }}
+    }}
+    """
+    # balance before listing
+    
+
+    query = gql(queryString)
+
+    result = await client.execute_async(query)
+    
+    days = {}
+
+    for res in result['wallets']:
+        for day in res['dailyBalance']:
+            if (int(day['day']) >= day_start and int(day['day']) <= (day_start+period)):
+                if not (str(day['day']) in days):
+                    days[str(day['day'])] = {}
+                    days[str(day['day'])]['timestamp'] = 1609459200 + 86400*int(day['day'])
+                    days[str(day['day'])]['balance'] = int(day['ohmBalance']) / 100000000
+                else:
+                    days[str(day['day'])]['timestamp'] = 1609459200 + 86400*int(day['day'])
+                    temp = days[str(day['day'])]['balance']
+                    temp += (int(day['ohmBalance'])/ 100000000)
+                    days[str(day['day'])]['balance'] = temp
+
+    days_array = []
+    for i in days:
+        days_array.append(days[i])
+
+    return days_array
+
+N = 10
+timestamp_start = 1617291702
+days = 15
+#wallet = "0x0822f3c03dcc24d200aff33493dc08d0e1f274a2"
+res = getFirstWallets(timestamp_start, days, N)
+
+print(res)
+
+
+
+
+
+
