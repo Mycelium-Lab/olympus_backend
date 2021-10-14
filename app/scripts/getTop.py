47eb668e1d491209110c09b2267fba7695077842
Deltax2016 <deltaxussr@gmail.com>
2021-10-14 17:35:24 +0300

    fixed '5 may' bug

--- app/scripts/getTop.py
+++ app/scripts/getTop.py
@@ -2,7 +2,7 @@ from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 from datetime import datetime
 
-transport = AIOHTTPTransport(url="https://api.thegraph.com/subgraphs/id/QmThQWLEohGNnWzKpozLxdDq5XHpWupyrJixWACbKSVMPM")
+transport = AIOHTTPTransport(url="https://api.thegraph.com/subgraphs/id/QmbuC5GvyA9eFq8vKBAvEj4ZRjSdbzrW9WgeSqtyMW3dM2")
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
 async def getTopBalances(timestamp_start, period, balance_gt):
@@ -10,6 +10,7 @@ async def getTopBalances(timestamp_start, period, balance_gt):
     balance_gt = balance_gt*1000000000
     day_start = datetime.fromtimestamp(timestamp_start).timetuple().tm_yday
     timestamp_end = timestamp_start + 86400*period
+    print(timestamp_end)
 
     queryString = f"""query getTopBalances {{
         t1:dailyBalances(first:1000,orderBy: timestamp, where: {{ohmBalance_gt: "{balance_gt}", timestamp_gt: "{timestamp_start}", timestamp_lt: "{timestamp_end}"}}) {{
@@ -23,6 +24,21 @@ async def getTopBalances(timestamp_start, period, balance_gt):
             day
         }}
         t3:dailyBalances(first:1000, skip: 2000, orderBy: timestamp, where: {{ohmBalance_gt: "{balance_gt}", timestamp_gt: "{timestamp_start}", timestamp_lt: "{timestamp_end}"}}) {{
+            ohmBalance
+            address
+            day
+        }}
+        t4:dailyBalances(first:1000, skip: 3000, orderBy: timestamp, where: {{ohmBalance_gt: "{balance_gt}", timestamp_gt: "{timestamp_start}", timestamp_lt: "{timestamp_end}"}}) {{
+            ohmBalance
+            address
+            day
+        }}
+        t5:dailyBalances(first:1000, skip: 4000, orderBy: timestamp, where: {{ohmBalance_gt: "{balance_gt}", timestamp_gt: "{timestamp_start}", timestamp_lt: "{timestamp_end}"}}) {{
+            ohmBalance
+            address
+            day
+        }}
+        t6:dailyBalances(first:1000, skip: 5000, orderBy: timestamp, where: {{ohmBalance_gt: "{balance_gt}", timestamp_gt: "{timestamp_start}", timestamp_lt: "{timestamp_end}"}}) {{
             ohmBalance
             address
             day
