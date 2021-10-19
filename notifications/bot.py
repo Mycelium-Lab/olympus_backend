d69538d7870cb3954771635af0be214cf651b85c
Deltax2016 <deltaxussr@gmail.com>
2021-10-19 17:17:42 +0300

    new url to ethereum scanner

--- notifications/bot.py
+++ notifications/bot.py
@@ -54,7 +54,7 @@ async def unstake(amount,to,tx):
     db = eval(file_db.read())
     file_db.close()
     for i in db:
-        await bot.send_message(i, f"🟨 Warning: Big unstake {amount} OHM to {to} \n\nWallet: https://etherscan.io/address/{to}")
+        await bot.send_message(i, f"🟨 Warning: Big unstake {amount} OHM to {to} \n\nWallet: https://ethplorer.io/ru/address/{to}")
     return "ok"
 
 async def transfer(amount,froms,to,tx):
@@ -62,7 +62,7 @@ async def transfer(amount,froms,to,tx):
     db = eval(file_db.read())
     file_db.close()
     for i in db:
-        await bot.send_message(i, f"🟦 Info: Big transfer {amount} OHM from {froms} to {to} \n\nTransaction: https://etherscan.io/tx/{tx}")
+        await bot.send_message(i, f"🟦 Info: Big transfer {amount} OHM from {froms} to {to} \n\nTransaction: https://ethplorer.io/ru/tx/{tx}")
     return "ok"
 
 async def transfer_dao(amount,froms,to,tx):
@@ -70,7 +70,7 @@ async def transfer_dao(amount,froms,to,tx):
     db = eval(file_db.read())
     file_db.close()
     for i in db:
-        await bot.send_message(i, f"🟥 Danger: Big transfer {amount} OHM from DAO to {to} \n\nTransaction: https://etherscan.io/tx/{tx}")
+        await bot.send_message(i, f"🟥 Danger: Big transfer {amount} OHM from DAO to {to} \n\nTransaction: https://ethplorer.io/ru/tx/{tx}")
     return "ok"
 
 async def minter(address):
