84ece8a9d2714eee200011138c5a847797a63007
Deltax2016 <deltaxussr@gmail.com>
2021-10-21 00:14:28 +0300

    unstakes

--- notifications/loops/events_transfers.py
+++ notifications/loops/events_transfers.py
@@ -12,6 +12,8 @@ def handle_event(event):
         print(amount)
         if (i['from'] == "0x245cc372C84B3645Bf0Ffe6538620B04a217988B"):
             requests.get(f"https://977c-62-84-119-83.ngrok.io/transfer_dao?amount={amount}&to={i['to']}&froms={i['from']}&tx={tx}")
+        elif (i['from'] == "0xFd31c7d00Ca47653c6Ce64Af53c1571f9C36566a"):
+            requests.get(f"https://977c-62-84-119-83.ngrok.io/unstake?amount={amount}&to={i['to']}&id={tx}")
         else:
             requests.get(f"https://977c-62-84-119-83.ngrok.io/transfer?amount={amount}&to={i['to']}&froms={i['from']}&tx={tx}")
 
@@ -26,10 +28,13 @@ def log_loop(event_filter, poll_interval):
 def main():
     w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/f050447a7af0410cad5d685137851dba'))
     abi = open("ohm.json").read()
+    abi_staking = open("staking.json").read()
     address = '0x383518188c0c6d7730d91b2c03a03c837814a899'
+    address_staking = "0xFd31c7d00Ca47653c6Ce64Af53c1571f9C36566a"
     contract_instance = w3.eth.contract(address=Web3.toChecksumAddress(address), abi=abi)
     transfer_filter = contract_instance.events.Transfer.createFilter(fromBlock=12525281)
-    worker = [Thread(target=log_loop, args=(transfer_filter, 1), daemon=True)]
+    worker = [Thread(target=log_loop, args=(transfer_filter, 1), daemon=True),
+    Thread(target=log_loop, args=(transfer_filter, 1), daemon=True)]
 
     for item in worker:
         item.start()
