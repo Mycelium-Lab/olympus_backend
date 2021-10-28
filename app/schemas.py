1f116d59d4002f53e3fcb85c334bcbb9b54fc3c2
Deltax2016 <deltaxussr@gmail.com>
2021-10-28 18:22:43 +0300

    oauth2 schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,39 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+from pydantic import UUID4, BaseModel, EmailStr, validator, Field
 
 
 class CreateUserRequest(BaseModel):
     login: str = " "
     password: str = " "
     telegram: str = " "
+
+class TokenBase(BaseModel):
+    token: UUID4 = Field(..., alias="access_token")
+    expires: datetime
+    token_type: Optional[str] = "bearer"
+
+    class Config:
+        allow_population_by_field_name = True
+
+    @validator("token")
+    def hexlify_token(cls, value):
+        return value.hex
+
+
+class UserBase(BaseModel):
+    id: int
+    email: EmailStr
+    name: str
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    name: str
+    password: str
+
+
+class User(UserBase):
+    token: TokenBase = {}
