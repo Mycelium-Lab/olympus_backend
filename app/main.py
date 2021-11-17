67e10e8eca8cc0833656275cfce6fee321758c1f
Deltax2016 <deltaxussr@gmail.com>
2021-11-17 14:20:23 +0300

    general analytics

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.scripts.getTop import getTopBalances
 from app.scripts.getBalance import getBalances
 from app.scripts.firstN import getFirstWallets, getFirstWalletsNDays, getFirstWalletsNHours, getFirstLegacy
 from app.scripts.indexes import parseNDays, parseNHours, parseNMinutes
+from app.scripts.general import parseGANDays, parseGANHours, parseGANMinutes
 from app.scripts.rebases import rebaseTimestamps
 from app.scripts.getTotal import totalWallets, totalBalances
 from fastapi.middleware.cors import CORSMiddleware
@@ -116,6 +117,21 @@ async def get_indexes_n_days(start: int = 1617291702, end: int = 1636458860, n: 
 @app.get("/api/get_index_n_hours")
 async def get_index_n_hours(start: int = 1623700800, end: int = 1623906000, n: int = 1):
     response  = await parseNHours(start, end, n)
+    return {"data":response}
+
+@app.get("/api/get_ga_n_minutes")
+async def get_index_n_minutes(start: int = 1623702000, end: int = 1623907200, n: int = 1, types: str = 'marketCapacity'):
+    response  = await parseGANMinutes(start, end, n, types)
+    return {"data":response}
+
+@app.get("/api/get_ga_n_days")
+async def get_indexes_n_days(start: int = 1617291702, end: int = 1636458860, n: int = 1, types: str = 'marketCapacity'):
+    response  = await parseGANDays(start, end, n, types)
+    return {"data":response}
+
+@app.get("/api/get_ga_n_hours")
+async def get_index_n_hours(start: int = 1623700800, end: int = 1623906000, n: int = 1, types: str = 'marketCapacity'):
+    response  = await parseGANHours(start, end, n, types)
     return {"data":response}
 
 @app.get("/api/get_index_n_minutes")
